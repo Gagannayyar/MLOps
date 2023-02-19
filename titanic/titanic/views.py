bc11c286f025561e41df76fe76a4fa7ed2e47581
Gagan Nayyar <gagan.nayyar@outlook.com>
2023-02-19 11:18:54 +0000

    Added Django

---
+++ titanic/titanic/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index.html')
+
+def results(request):
+    user_input_age = request.GET.get("age")
+    return render(request, 'results.html', {'age': user_input_age})
